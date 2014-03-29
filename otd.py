85ce68ae0d08f3dabe5c318e6b0f0cfc96c33a91
Jeffrey Tang <jeffrey.tang@yandex.com>
2014-03-29 11:02:44 -0500

    Add favicon

--- otd.py
+++ otd.py
@@ -7,6 +7,10 @@ from bson.objectid import ObjectId
 app = flask.Flask(__name__)
 app.db = MongoClient(os.getenv('MONGOHQ_URL')).otd
 
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
+
 @app.route('/')
 def index():
     return '<html><body>Try <a href="/covers/5334c81a8f51ef1966b82cd6">Earth Science</a>. We&apos;re getting there!</body></html>'
