8f4ab332c498682eff074b77050a5f5a2d919cbf
Jeffrey Tang <jeffrey.tang@yandex.com>
2014-03-30 15:01:05 -0500

    Add glencoe_lit resolve

--- resolve.py
+++ resolve.py
@@ -11,10 +11,20 @@ def hrw(document):
     except KeyError:
         raise InvalidFormat
 
-    data['url'] = 'http://my.hrw.com/tabnav/controller.jsp?isbn=' + document['isbn_10']
+    data['url'] = 'http://my.hrw.com/tabnav/controller.jsp?isbn=' + isbn_10
     return flask.render_template('post.html',
             title=title, action='http://my.hrw.com/index.jsp', data=data)
 
+def glencoe_literature(document):
+    try:
+        title = document['title']
+        isbn_13 = document['isbn_13']
+    except KeyError:
+        raise InvalidFormat
+
+    return flask.redirect('http://www.glencoe.com/ebooks/literature/{}/glencoe_main.html'.format(isbn_13))
+
 convert = {
     'hrw': hrw,
+    'glencoe_lit': glencoe_literature,
 }
