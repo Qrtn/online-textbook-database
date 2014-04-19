991a637dc46763f3b04860b0bae00732d1eb5038
Jeffrey Tang <jeffrey.tang@yandex.com>
2014-04-19 10:10:27 -0500

    Add glencoe_showbook, glencoe_pdf_la

--- resolve.py
+++ resolve.py
@@ -23,7 +23,27 @@ def glencoe_swf_lit(**kwargs):
 
     return flask.redirect('http://www.glencoe.com/ebooks/literature/{}/glencoe_main.html'.format(isbn_13))
 
+def glencoe_showbook(**kwargs):
+    try:
+        title = kwargs['document']
+        data = {'access_code': kwargs['document']['access']['glencoe_showbook'][kwargs['index']]}
+    except KeyError:
+        raise InvalidFormat
+
+    return flask.render_template('post.html',
+        title=title, action='http://www.glencoe.com/ose/showbook.php', data=data)
+
+def glencoe_pdf_la(**kwargs):
+    try:
+        isbn_13 = kwargs['document']['isbn_13']
+    except KeyError:
+        raise InvalidFormat
+
+    return flask.redirect('http://www.glencoe.com/cgi-bin/pdfServer.pl/ebooks/language_arts/{}/swopen.pdf'.format(isbn_13))
+
 convert = {
     'hrw': hrw,
     'glencoe_swf_lit': glencoe_swf_lit,
+    'glencoe_showbook': glencoe_showbook,
+    'glencoe_pdf_la': glencoe_pdf_la,
 }
