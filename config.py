351348684a3455b99627ff90b0cd06657028ea9d
Jeffrey Tang <jeffrey.tang@yandex.com>
2014-10-03 18:37:01 -0500

    Fix Google Analytics script injection
    Inject script when not in development! Was injecting script only in
    development.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ DEBUG = development
 
 MONGOLAB_URI = os.getenv('MONGOLAB_URI')
 S3_BUCKET = os.getenv('S3_BUCKET')
-GA_TRACKING_ID = development and os.getenv('GA_TRACKING_ID')
+GA_TRACKING_ID = not development and os.getenv('GA_TRACKING_ID')
 
 CLASSZONE_QRTN_PASSWORD = os.getenv('CLASSZONE_QRTN_PASSWORD')
 QRTN_DROPBOX_UID = os.getenv('QRTN_DROPBOX_UID')
