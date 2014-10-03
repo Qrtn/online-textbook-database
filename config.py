7a49a3f57c592bf77a023f319eff0c36cadc5e76
Jeffrey Tang <jeffrey.tang@yandex.com>
2014-10-03 17:54:36 -0500

    Add Google Analytics
    analytics.html is template, included in index.html and post.html.
    analytics.html takes Tracking ID from config.py, which takes it from
    environment variable. config.GA_TRACKING_ID => $GA_TRACKING_ID

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ development = os.getenv('FLASK_ENV') == 'development'
 DEBUG = development
 
 MONGOLAB_URI = os.getenv('MONGOLAB_URI')
+GA_TRACKING_ID = development and os.getenv('GA_TRACKING_ID')
 
 CLASSZONE_QRTN_PASSWORD = os.getenv('CLASSZONE_QRTN_PASSWORD')
 QRTN_DROPBOX_UID = os.getenv('QRTN_DROPBOX_UID')
